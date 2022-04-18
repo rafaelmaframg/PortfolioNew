c13fdbfc9d76a43b587d640c975af9504d6d3ef0
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-04-18 20:55:12 +0100

    removendo secret key e gerando nova atravez do shell

--- rafaelmafra/settings.py
+++ rafaelmafra/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nm&pmf^+!+cis3ak6(w3eds6@@%@)%_q)hcaqgfi10@(p^_^9s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
